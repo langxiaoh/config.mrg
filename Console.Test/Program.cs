7e2f1c31e8a01ff2d98d650a1e37ddb4c62caa6a
langxiaohua <langxiaoh@163.com>
2019-08-10 21:45:45 +0800

    不暂存提交

--- Console.Test/Program.cs
+++ Console.Test/Program.cs
@@ -19,6 +19,8 @@ namespace Console.Test
             System.Console.WriteLine("再测试一下暂存后提交");
             System.Console.WriteLine("看看提交前两次暂存");
 
+            System.Console.WriteLine("测试依稀撤销");
+            System.Console.WriteLine("撤销暂存");
             
         }
     }
